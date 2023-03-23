c76abb404e506f7ccf8d58b743a50ea7d42aa2df
Frank-Duffy <fduffy0328@gmail.com>
2023-03-23 14:08:58 -0400

    updated readme and licensing

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-# main.py
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Authors - Frank Duffy, LaBEQ / Sardashti Research Group, Clemson University
+#
+# Based on tutorials about matplotlib animate by Corey Schafer and multiprocessing
+# by Engineer Man
+#
+# Distributed under the terms of the BSD license.
+#
+# The full license is in the file LICENCE, distributed with this software.
+# -----------------------------------------------------------------------------
+
 import enaml
 from enaml.qt.qt_application import QtApplication
 
